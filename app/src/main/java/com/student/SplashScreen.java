ed8dae826ee482bc071794591555cccb924cb8f2
unknown <wojciech.grodecki@etongroup.eu>
2018-10-10 10:13:25 +0200

    added permission&splash screen

---
+++ app/src/main/java/com/student/SplashScreen.java
@@ -0,0 +1,62 @@
+package com.student;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SplashScreen extends AppCompatActivity {
+    private static final int DELAY =1700;
+    private final static int REQUEST_CODE_ASK_PERMISSIONS = 101;
+    private static final String[] REQUIRED_PERMISSIONS = new String[] {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        checkPermissions();
+    }
+
+    private void checkPermissions() {
+        List<String> missingPermissions = new ArrayList<>();
+
+        for (final String permission : REQUIRED_PERMISSIONS) {
+            final int result = ContextCompat.checkSelfPermission(this, permission);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                missingPermissions.add(permission);
+            }
+        }
+
+        if (!missingPermissions.isEmpty()) {
+            String[] permissions = missingPermissions
+                    .toArray(new String[missingPermissions.size()]);
+            ActivityCompat.requestPermissions(this, permissions,
+                    REQUEST_CODE_ASK_PERMISSIONS);
+        } else {
+            startTimeOutActivity();
+        }
+    }
+
+
+
+    private void startWelcomeScreen(){
+        Intent intent = new Intent(this,WelcomeScreen.class);
+        startActivity(intent);
+    }
+
+    private void startTimeOutActivity(){
+        Handler handler = new Handler();
+        handler.postDelayed(this::startWelcomeScreen, DELAY);
+
+    }
+}
